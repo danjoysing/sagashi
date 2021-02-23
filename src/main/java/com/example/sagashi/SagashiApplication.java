f35809709121bacd7cf79e806b7cf3c4ae3d2d7a
tienan.chien <tienan.chien@iisigroup.com>
2021-02-23 13:57:19 +0800

    init

---
+++ src/main/java/com/example/sagashi/SagashiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagashiApplication.class, args);
+	}
+
+}
